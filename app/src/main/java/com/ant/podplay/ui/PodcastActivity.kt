e91435a33304614d26e909e86f60e288bde91035
anthisse <ant@centripetalfusion.com>
2023-04-29 13:03:17 -0400

    Create podcast search bar

--- app/src/main/java/com/ant/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/ant/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,12 @@
 package com.ant.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.ant.podplay.R
 import com.ant.podplay.repository.ItunesRepo
 import com.ant.podplay.service.ItunesService
@@ -25,5 +29,24 @@ class PodcastActivity : AppCompatActivity() {
             val results = itunesRepo.searchByTerm("Android Developer")
             Log.i(TAG, "Results = ${results.body()}")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the options menu
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        // Replaced with safe call
+        // Find the search item and set a search view from the item's actionView property
+        val searchMenuItem = menu?.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        // Load the SearchManager
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        // Load the search configuration and assign it to the searchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 }
