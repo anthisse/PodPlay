21f0574567ddf47232063638d2987a62c7dde76b
anthisse <ant@centripetalfusion.com>
2023-05-03 10:20:01 -0400

    Create minimal search functionality

--- app/src/main/java/com/ant/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/ant/podplay/ui/PodcastActivity.kt
@@ -2,10 +2,11 @@ package com.ant.podplay.ui
 
 import android.app.SearchManager
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import com.ant.podplay.R
 import com.ant.podplay.repository.ItunesRepo
@@ -15,20 +16,12 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class PodcastActivity : AppCompatActivity() {
-    val TAG = javaClass.simpleName
+    private val TAG = javaClass.simpleName
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
-
-        val itunesService = ItunesService.instance
-        val itunesRepo = ItunesRepo(itunesService)
-
-        GlobalScope.launch {
-            val results = itunesRepo.searchByTerm("Android Developer")
-            Log.i(TAG, "Results = ${results.body()}")
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -46,7 +39,35 @@ class PodcastActivity : AppCompatActivity() {
 
         // Load the search configuration and assign it to the searchView
         searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
-
         return true
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    // Search for a term
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun performSearch(term: String) {
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm(term)
+            Log.i(TAG, "Results = ${results.body()}")
+        }
+    }
+
+    // Handle incoming intents
+    // Replaced with null safety
+    private fun handleIntent(intent: Intent?) {
+        // If the intent is an ACTION_SEARCH
+        if (Intent.ACTION_SEARCH == intent?.action) {
+            // Extract the search query and pass it to performSearch()
+            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+            performSearch(query)
+        }
     }
 }
